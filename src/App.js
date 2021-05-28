13941c04ec2798a769cf9c7fdb7f73235771bff9
Luiz <lvj253@gmail.com>
2021-05-27 22:51:12 -0300

    estilos padrões

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import {BrowserRouter, Route, Switch } from 'react-router-dom';
-
+import GlobalStyle from './components/utils/GlobalStyle';
 
 export default function App() {
   return (
    <BrowserRouter>
+    <GlobalStyle/>
    
-   <Switch>
+    <Switch>
 
-   </Switch>
+    </Switch>
    </BrowserRouter>
   );
 }
