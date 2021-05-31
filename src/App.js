bfc5f2a87482efa3cbf7fe2e703bd75e97d5fdbf
Daniel Luiz <danielluizthiessen@gmail.com>
2021-05-30 21:26:20 -0300

    Criei arquivos e iniciei desenvolvimento

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import {BrowserRouter, Route, Switch } from 'react-router-dom';
 import GlobalStyle from './components/utils/GlobalStyle';
+import MyPosts from "./components/MyPosts.js";
+import Header from "./components/utils/Header";
+
 
 export default function App() {
   return (
    <BrowserRouter>
     <GlobalStyle/>
-   
+    <Header></Header>
     <Switch>
-
+      <Route path="/myposts" exact={true} component={MyPosts} />
     </Switch>
    </BrowserRouter>
   );
