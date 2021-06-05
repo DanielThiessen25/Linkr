58f55dc8d714ea25a0e5af39ea5d4c79d918f57d
Luiz <lvj253@gmail.com>
2021-06-05 14:31:58 -0300

    pequena config

--- src/components/Hashtags/Hashtags.js
+++ src/components/Hashtags/Hashtags.js
@@ -24,6 +24,7 @@ export default function Hashtags(props){
 
     function linkTypedHash(e){
         e.preventDefault();
+        setTypedHash("");
         const hashTrim = typedHash.trim();
         if(hashTrim.length === 0){
             return;
